89c4bf3634e9a3de861591cd0c973d590ac5619b
Niraj Dev <devniraj4@gmail.com>
2026-07-16 22:58:49 +0545

    Configure STATIC_ROOT for render collection

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,7 +118,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 #LOGIN & LOGOUT REDIRECTS
 LOGIN_REDIRECT_URL = '/'
