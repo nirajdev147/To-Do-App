5ef1cf95d5998e556155973082836b99eb3c7096
Niraj Dev <devniraj4@gmail.com>
2026-07-17 00:46:22 +0545

    Wrap WSGI application with WhiteNoise for strict MIME types

--- todo_app/wsgi.py
+++ todo_app/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_app.settings')
 
 application = get_wsgi_application()
+
+# This forces the Gunicorn web server to serve static assets through WhiteNoise directly
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'staticfiles'))
